9d788aefca3adbaf4c7674cc7bd154b314cf1097
David Mulder <dmulder@samba.org>
2024-11-15 15:08:45 -0700

    Add FIDO2 example code
    Signed-off-by: David Mulder <dmulder@samba.org>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use himmelblau::error::MsalError;
-use himmelblau::{BrokerClientApplication, EnrollAttrs};
+use himmelblau::{AuthOption, BrokerClientApplication, EnrollAttrs, MFAAuthContinue};
 use kanidm_hsm_crypto::soft::SoftTpm;
 use kanidm_hsm_crypto::{AuthValue, BoxedDynTpm, Tpm};
 use rpassword::read_password;
@@ -8,12 +8,28 @@ use std::io::Write;
 use std::str::FromStr;
 use std::thread::sleep;
 use std::time::Duration;
-use tracing::Level;
+use tracing::{error, Level};
 use tracing_subscriber::FmtSubscriber;
 
 use anyhow::{anyhow, Result};
 use reqwest::Url;
 use serde::Deserialize;
+
+use authenticator::{
+    authenticatorservice::{AuthenticatorService, SignArgs},
+    ctap2::server::{
+        AuthenticationExtensionsClientInputs, PublicKeyCredentialDescriptor,
+        UserVerificationRequirement,
+    },
+    statecallback::StateCallback,
+    Pin, StatusPinUv, StatusUpdate,
+};
+use base64::engine::general_purpose::URL_SAFE_NO_PAD;
+use base64::Engine;
+use serde_json::{json, to_string as json_to_string};
+use sha2::{Digest, Sha256};
+use std::sync::mpsc::{channel, RecvError};
+use std::thread;
 
 #[derive(Debug, Deserialize)]
 struct FederationProvider {
@@ -54,6 +70,157 @@ fn split_username(username: &str) -> Option<(&str, &str)> {
         return Some((tup[0], tup[1]));
     }
     None
+}
+
+async fn fido_auth(flow: &MFAAuthContinue) -> Result<String, Box<dyn std::error::Error>> {
+    // Initialize AuthenticatorService
+    let mut manager = AuthenticatorService::new()?;
+    manager.add_u2f_usb_hid_platform_transports();
+
+    let fido_challenge = flow
+        .fido_challenge
+        .clone()
+        .ok_or("sFidoChallenge missing from response")?;
+
+    let challenge_str = json_to_string(&json!({
+        "type": "webauthn.get",
+        "challenge": URL_SAFE_NO_PAD.encode(fido_challenge.clone()),
+        "origin": "https://login.microsoft.com"
+    }))?;
+
+    // Create a channel for status updates
+    let (status_tx, status_rx) = channel::<StatusUpdate>();
+    thread::spawn(move || loop {
+        match status_rx.recv() {
+            Ok(StatusUpdate::InteractiveManagement(..)) => {
+                panic!("STATUS: This can't happen when doing non-interactive usage");
+            }
+            Ok(StatusUpdate::SelectDeviceNotice) => {
+                println!("STATUS: Please select a device by touching one of them.");
+            }
+            Ok(StatusUpdate::PresenceRequired) => {
+                println!("STATUS: waiting for user presence");
+            }
+            Ok(StatusUpdate::PinUvError(StatusPinUv::PinRequired(sender))) => {
+                let raw_pin =
+                    rpassword::prompt_password("Enter PIN: ").expect("Failed to read PIN");
+                sender.send(Pin::new(&raw_pin)).expect("Failed to send PIN");
+                continue;
+            }
+            Ok(StatusUpdate::PinUvError(StatusPinUv::InvalidPin(sender, attempts))) => {
+                println!(
+                    "Wrong PIN! {}",
+                    attempts.map_or("Try again.".to_string(), |a| format!(
+                        "You have {a} attempts left."
+                    ))
+                );
+                let raw_pin =
+                    rpassword::prompt_password("Enter PIN: ").expect("Failed to read PIN");
+                sender.send(Pin::new(&raw_pin)).expect("Failed to send PIN");
+                continue;
+            }
+            Ok(StatusUpdate::PinUvError(StatusPinUv::PinAuthBlocked)) => {
+                panic!("Too many failed attempts in one row. Your device has been temporarily blocked. Please unplug it and plug in again.")
+            }
+            Ok(StatusUpdate::PinUvError(StatusPinUv::PinBlocked)) => {
+                panic!("Too many failed attempts. Your device has been blocked. Reset it.")
+            }
+            Ok(StatusUpdate::PinUvError(StatusPinUv::InvalidUv(attempts))) => {
+                println!(
+                    "Wrong UV! {}",
+                    attempts.map_or("Try again.".to_string(), |a| format!(
+                        "You have {a} attempts left."
+                    ))
+                );
+                continue;
+            }
+            Ok(StatusUpdate::PinUvError(StatusPinUv::UvBlocked)) => {
+                println!("Too many failed UV-attempts.");
+                continue;
+            }
+            Ok(StatusUpdate::PinUvError(e)) => {
+                panic!("Unexpected error: {:?}", e)
+            }
+            Ok(StatusUpdate::SelectResultNotice(_, _)) => {
+                panic!("Unexpected select device notice")
+            }
+            Err(RecvError) => {
+                println!("STATUS: end");
+                return;
+            }
+        }
+    });
+
+    let fido_allow_list = flow
+        .fido_allow_list
+        .clone()
+        .ok_or("arrFidoAllowList missing from response".to_string())?;
+
+    let allow_list: Vec<PublicKeyCredentialDescriptor> = fido_allow_list
+        .into_iter()
+        .filter_map(|id| match URL_SAFE_NO_PAD.decode(id) {
+            Ok(decoded_id) => Some(PublicKeyCredentialDescriptor {
+                id: decoded_id,
+                transports: vec![],
+            }),
+            Err(e) => {
+                error!("Failed decoding allow list id: {:?}", e);
+                None
+            }
+        })
+        .collect();
+
+    // Prepare SignArgs
+    let chall_bytes = Sha256::digest(challenge_str.clone()).into();
+    let ctap_args = SignArgs {
+        client_data_hash: chall_bytes,
+        origin: "https://login.microsoft.com".to_string(),
+        relying_party_id: "login.microsoft.com".to_string(),
+        allow_list,
+        user_verification_req: UserVerificationRequirement::Preferred,
+        user_presence_req: true,
+        extensions: AuthenticationExtensionsClientInputs::default(),
+        pin: None,
+        use_ctap1_fallback: false,
+    };
+
+    // Perform authentication
+    let (sign_tx, sign_rx) = channel();
+    let callback = StateCallback::new(Box::new(move |rv| {
+        sign_tx.send(rv).unwrap();
+    }));
+
+    match manager.sign(25000, ctap_args, status_tx.clone(), callback) {
+        Ok(_) => (),
+        Err(e) => panic!("Couldn't sign: {:?}", e),
+    }
+
+    let assertion_result = sign_rx.recv()??;
+
+    let credential_id = assertion_result
+        .assertion
+        .credentials
+        .as_ref()
+        .map(|cred| cred.id.clone())
+        .unwrap_or_default();
+    let auth_data = assertion_result.assertion.auth_data;
+    let signature = assertion_result.assertion.signature;
+    let user_handle = assertion_result
+        .assertion
+        .user
+        .as_ref()
+        .map(|user| user.id.clone())
+        .unwrap_or_default();
+    let json_response = json!({
+        "id": URL_SAFE_NO_PAD.encode(credential_id),
+        "clientDataJSON": URL_SAFE_NO_PAD.encode(challenge_str),
+        "authenticatorData": URL_SAFE_NO_PAD.encode(auth_data.to_vec()),
+        "signature": URL_SAFE_NO_PAD.encode(signature),
+        "userHandle": URL_SAFE_NO_PAD.encode(user_handle),
+    });
+
+    // Convert the JSON response to a string
+    Ok(json_to_string(&json_response).unwrap())
 }
 
 #[tokio::main]
@@ -129,7 +296,11 @@ async fn main() {
         }
     };
     let mut mfa_req = match app
-        .initiate_acquire_token_by_mfa_flow_for_device_enrollment(&username, &password)
+        .initiate_acquire_token_by_mfa_flow_for_device_enrollment(
+            &username,
+            &password,
+            vec![AuthOption::Fido],
+        )
         .await
     {
         Ok(mfa) => mfa,
@@ -141,6 +312,26 @@ async fn main() {
     print!("{}", mfa_req.msg);
 
     let token1 = match mfa_req.mfa_method.as_str() {
+        "FidoKey" => {
+            // Create the assertion
+            let assertion = match fido_auth(&mfa_req).await {
+                Ok(assertion) => assertion,
+                Err(e) => {
+                    println!("FIDO ASSERTION FAIL: {:?}", e);
+                    return ();
+                }
+            };
+            match app
+                .acquire_token_by_mfa_flow(&username, Some(&assertion), None, &mut mfa_req)
+                .await
+            {
+                Ok(token) => token,
+                Err(e) => {
+                    println!("MFA FAIL: {:?}", e);
+                    return ();
+                }
+            }
+        }
         "PhoneAppOTP" | "OneWaySMS" | "ConsolidatedTelephony" => {
             io::stdout().flush().unwrap();
             let input = match read_password() {
@@ -260,7 +451,7 @@ async fn main() {
         token4.access_token.clone().unwrap(),
         token4.spn().unwrap(),
         token4.uuid().unwrap(),
-        token.amr_mfa().unwrap()
+        token4.amr_mfa().unwrap()
     );
 
     let _prt = match &token4.prt {
